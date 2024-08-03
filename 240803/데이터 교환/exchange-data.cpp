6313105a3bb187684cc30201c86d6404c919e67b
김이정 <133465838+shashamalone@users.noreply.github.com>
2024-08-03 13:52:53 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240803/데이터 교환/exchange-data.cpp
+++ 240803/데이터 교환/exchange-data.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main() {
     int a = 5, b= 6, c=7;
-    int temp1, temp2;
+    int temp = a;
 
-    temp1 = b;
-    temp2 = c;
-    b = a;
-    c = temp1 ;
-    a = temp2;
+    a = c; 
+    c = b;    
+    b = temp;
+    
+
     
     cout << a << endl << b << endl << c;
     return 0;
